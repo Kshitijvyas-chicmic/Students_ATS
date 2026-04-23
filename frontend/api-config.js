bcb2923cbf6a784962fe18479c63a8b7f3ac6a15
princeSinglacoder <princesingla8570@gmail.com>
2026-04-23 16:45:28 +0530

    update backend render url

--- frontend/api-config.js
+++ frontend/api-config.js
@@ -4,7 +4,7 @@
  * Using 127.0.0.1 instead of localhost to avoid potential name resolution issues.
  */
 
-const API_BASE_URL = 'https://student-ats-1.onrender.com'; 
+const API_BASE_URL = 'https://student-ats-2.onrender.com'; 
 
 window.API_BASE_URL = API_BASE_URL;
 console.log("ATS App initialized with API_BASE_URL:", window.API_BASE_URL);
